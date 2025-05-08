d1a3711b036759735638f8342020579a6d4d6d6b
jballesta2001 <josemanuel2001135@gmail.com>
2025-05-08 10:32:38 +0200

    poner navbar global

--- frontend/frontend/src/App.vue
+++ frontend/frontend/src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
-    <router-view /> <!- Aquí se renderizarán tus componentes de ruta -->
+    <NavbarModule /> <!-- Navbar global -->
+    <router-view /> <!-- Aquí se renderizarán tus componentes de ruta -->
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'App',
-  };
+import NavbarModule from './components/modules/NavbarModule.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavbarModule,
+  },
+};
 </script>
 
 <style>
